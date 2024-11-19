d38fcd7952fabd65e88fb6571e4b7892d3de079b
fangzheng <fangzheng@nrp-pet.cn>
2024-11-19 17:56:33 +0800

    add

--- src/api/apis.ts
+++ src/api/apis.ts
@@ -1,6 +1,7 @@
 import type { DescData } from '@arco-design/web-vue/es/descriptions/interface' // 类型导入应放在最前
 import axios from 'axios'
 import qs from 'query-string'
+import { Dayjs } from 'dayjs'
 import { ApiResponse, PageDataResponse } from './types' // 非类型导入在后
 
 // ApiList 接口
@@ -53,18 +54,20 @@ export interface QueryResult {
     executionTime: number // SQL 执行时间，单位为毫秒
 }
 export interface ApiRequestLog {
-    id: number // 主键ID
+    id?: number // 主键ID
     apiId?: number // 关联的API ID
     apiName?: string // API名称
     apiPath?: string // API路径
     requestParams?: string // 请求参数
     responseData?: string // 响应数据
-    responseStatus: number // 响应状态码
+    responseStatus?: number // 响应状态码
     responseMessage?: string // 响应消息
     requestDuration?: number // 请求耗时（毫秒）
     clientIp?: string // 客户端IP
-    createdTime: string // 创建时间
+    createdTime?: string // 创建时间
+    createdTimeRange?: string[] // 创建时间范围，定义为 Dayjs 类型数组
 }
+
 export interface ApiRequestLogParams extends Partial<ApiRequestLog> {
     current: number // 当前页数
     pageSize: number // 每页条数
@@ -140,7 +143,7 @@ export function deleteApiSqlParam(id: number) {
 
 // API接口请求日志
 export function apiRequestLogList(params: ApiRequestLogParams) {
-    return axios.post<PageDataResponse<ApiRequestLog[]>>(`/api/apiRequestLog/list`, qs.stringify(params), {
+    return axios.post<PageDataResponse<ApiRequestLog>>(`/api/apiRequestLog/list`, qs.stringify(params), {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
--- src/views/api-manager/components/api-log.vue
+++ src/views/api-manager/components/api-log.vue
@@ -1,261 +1,239 @@
 <template>
-    <div class="api-log" v-if="api">
-        <div class="container">
-            <a-card class="general-card" title="访问日志列表">
-                <a-row>
-                    <a-col :flex="1">
-                        <a-form
-                            :model="formModel"
-                            :label-col-props="{ span: 6 }"
-                            :wrapper-col-props="{ span: 18 }"
-                            label-align="right"
-                        >
-                            <a-row :gutter="16">
-                                <!-- <a-col :span="6">
-                                    <a-form-item field="number" label="API ID">
-                                        <a-input v-model="formModel.number" placeholder="请输入API接口ID" />
-                                    </a-form-item>
-                                </a-col>
-                                <a-col :span="6">
-                                    <a-form-item field="name" label="API路径">
-                                        <a-input v-model="formModel.name" placeholder="请输入API接口路径" />
-                                    </a-form-item>
-                                </a-col> -->
-                                <a-col :span="6">
-                                    <a-form-item field="contentType" label="状态">
-                                        <a-select
-                                            v-model="formModel.contentType"
-                                            :options="contentTypeOptions"
-                                            placeholder="全部"
-                                        />
-                                    </a-form-item>
-                                </a-col>
-                                <a-col :span="6">
-                                    <a-form-item field="createdTime" label="时间">
-                                        <a-range-picker v-model="formModel.createdTime" style="width: 100%" />
-                                    </a-form-item>
-                                </a-col>
-                            </a-row>
-                        </a-form>
-                    </a-col>
-                    <a-col :flex="'86px'" style="text-align: right">
-                        <a-space direction="vertical" :size="18">
-                            <a-button type="primary" @click="search">
-                                <template #icon>
-                                    <icon-search />
-                                </template>
-                                查询
-                            </a-button>
-                        </a-space>
-                    </a-col>
-                </a-row>
-                <a-divider style="margin-top: 0" />
-                <a-table
-                    row-key="id"
-                    :loading="loading"
-                    :pagination="pagination"
-                    :data="renderData"
-                    :bordered="false"
-                    :size="size"
-                    @page-change="onPageChange"
-                >
-                    <a-table-column title="API ID" data-index="id" :width="50" :max-width="50"></a-table-column>
+    <div class="container" v-if="api">
+        <a-card class="general-card" title="日志记录">
+            <!-- 搜索表单 -->
+            <a-row>
+                <a-col :flex="1">
+                    <a-form
+                        :model="formModel"
+                        :label-col-props="{ span: 6 }"
+                        :wrapper-col-props="{ span: 18 }"
+                        label-align="right"
+                    >
+                        <a-row :gutter="16">
+                            <a-col :span="6">
+                                <a-form-item field="responseStatus" label="响应状态">
+                                    <a-select v-model="formModel.responseStatus" default-value="0">
+                                        <a-option value="0">全部</a-option>
+                                        <a-option value="200">成功200</a-option>
+                                        <a-option value="400">请求失败400</a-option>
+                                        <a-option value="500">服务器错误500</a-option>
+                                    </a-select>
+                                </a-form-item>
+                            </a-col>
+                            <a-col :span="12">
+                                <a-form-item field="remark" label="时间">
+                                    <a-range-picker
+                                        style="width: 100%"
+                                        show-time
+                                        format="YYYY-MM-DD HH:mm:ss"
+                                        v-model="formModel.createdTimeRange"
+                                    />
+                                </a-form-item>
+                            </a-col>
+                        </a-row>
+                    </a-form>
+                </a-col>
+                <a-col :flex="'86px'" style="text-align: right">
+                    <a-space direction="vertical" :size="18">
+                        <a-button type="primary" @click="search">
+                            <template #icon>
+                                <icon-search />
+                            </template>
+                            {{ $t('searchTable.form.search') }}
+                        </a-button>
+                    </a-space>
+                </a-col>
+            </a-row>
+            <a-divider style="margin-top: 0" />
+
+            <!-- 搜索结果表 -->
+            <a-table
+                row-key="id"
+                column-resizable
+                :loading="loading"
+                :pagination="pagination"
+                :data="renderData"
+                :bordered="false"
+                @page-change="onPageChange"
+            >
+                <template #columns>
+                    <a-table-column title="#" data-index="id" :width="50" :max-width="50"></a-table-column>
+                    <a-table-column title="API ID" data-index="apiId" :width="80" :max-width="80"></a-table-column>
                     <a-table-column title="API名称" data-index="apiName"></a-table-column>
-                    <a-table-column title="API路径" data-index="apiPath" :width="500" :minWidth="500"></a-table-column>
-                    <a-table-column title="请求耗时" data-index="requestDuration"></a-table-column>
-                    <a-table-column title="请求状态" data-index="requestStatus"></a-table-column>
-                    <a-table-column title="请求参数" data-index="requestParams"></a-table-column>
-                    <a-table-column title="响应内容" data-index="responseData"></a-table-column>
-                    <a-table-column title="时间" data-index="createdTime"></a-table-column>
-                </a-table>
-            </a-card>
-        </div>
+                    <a-table-column title="API路径" data-index="apiPath"></a-table-column>
+                    <a-table-column title="请求耗时" data-index="requestDuration" :width="100" :max-width="100"></a-table-column>
+                    <a-table-column title="请求IP" data-index="clientIp"></a-table-column>
+                    <a-table-column title="请求状态" data-index="responseStatus" :width="100" :max-width="100"></a-table-column>
+                    <a-table-column title="请求参数" data-index="requestParams" :ellipsis="true" :tooltip="true"></a-table-column>
+                    <a-table-column title="响应数据" data-index="responseData" :ellipsis="true" :tooltip="true"></a-table-column>
+                    <a-table-column title="创建时间" data-index="createdTime"></a-table-column>
+                </template>
+            </a-table>
+        </a-card>
     </div>
     <div class="no-api-doc" v-else>
         <p>请选择一个 API 以查看详情</p>
     </div>
 </template>
 
-<script setup lang="ts">
-import { ref, watch, reactive, getCurrentInstance, nextTick, computed, onMounted } from 'vue'
+<script lang="ts" setup>
+import { computed, ref, reactive, watch, inject, getCurrentInstance, nextTick, onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
 import useLoading from '@/hooks/loading'
+import { ApiList, ApiRequestLog, ApiRequestLogParams, apiRequestLogList } from '@/api/apis'
 import { Pagination } from '@/types/global'
-import type { SelectOptionData } from '@arco-design/web-vue/es/select/interface'
-import type { TableColumnData } from '@arco-design/web-vue/es/table/interface'
-import cloneDeep from 'lodash/cloneDeep'
-import Sortable from 'sortablejs'
-import { useRouter, useRoute } from 'vue-router'
-import { ApiList, ApiRequestLog, apiRequestLogList, ApiRequestLogParams } from '@/api/apis'
+import { Message } from '@arco-design/web-vue'
+import { Dayjs } from 'dayjs'
 
-type SizeProps = 'mini' | 'small' | 'medium' | 'large'
-type Column = TableColumnData & { checked?: true }
+const { proxy } = getCurrentInstance()!
+const baseURL = inject('baseURL')
 
-const generateFormModel = () => {
-    return {
-        number: '',
-        name: '',
-        contentType: '',
-        filterType: '',
-        createdTime: [],
-        status: ''
-    }
-}
-const { loading, setLoading } = useLoading(true)
-const { t } = useI18n()
-const renderData = ref<ApiRequestLog[]>([])
-const formModel = ref(generateFormModel())
-const cloneColumns = ref<Column[]>([])
-const showColumns = ref<Column[]>([])
-
-const size = ref<SizeProps>('medium')
-
-const basePagination: Pagination = {
-    current: 1,
-    pageSize: 20
-}
-const pagination = reactive({
-    ...basePagination
+// 表单模型
+const formModel = ref<ApiRequestLog>({
+    id: 0,
+    apiId: 0,
+    responseStatus: undefined,
+    createdTimeRange: [] // 初始化为空数组
 })
-const columns = computed<TableColumnData[]>(() => [
-    {
-        title: t('searchTable.columns.index'),
-        dataIndex: 'index',
-        slotName: 'index'
-    },
-    {
-        title: t('searchTable.columns.number'),
-        dataIndex: 'number'
-    },
-    {
-        title: t('searchTable.columns.name'),
-        dataIndex: 'name'
-    },
-    {
-        title: t('searchTable.columns.contentType'),
-        dataIndex: 'contentType',
-        slotName: 'contentType'
-    },
-    {
-        title: t('searchTable.columns.filterType'),
-        dataIndex: 'filterType'
-    },
-    {
-        title: t('searchTable.columns.count'),
-        dataIndex: 'count'
-    },
-    {
-        title: t('searchTable.columns.createdTime'),
-        dataIndex: 'createdTime'
-    },
-    {
-        title: t('searchTable.columns.status'),
-        dataIndex: 'status',
-        slotName: 'status'
-    },
-    {
-        title: t('searchTable.columns.operations'),
-        dataIndex: 'operations',
-        slotName: 'operations'
-    }
-])
-const contentTypeOptions = computed<SelectOptionData[]>(() => [
-    {
-        label: '全部',
-        value: ''
-    },
-    {
-        label: '成功',
-        value: '200'
-    },
-    {
-        label: '失败',
-        value: '500'
-    }
-])
-
-const fetchData = async (params: ApiRequestLogParams = { current: 1, pageSize: 20 }) => {
-    setLoading(true)
-    try {
-        const { data } = await apiRequestLogList(params)
-        renderData.value = data.records // 设置渲染数据
-        pagination.current = params.current // 更新当前页
-        pagination.total = data.total // 更新总条数
-    } catch (err) {
-        // you can report use errorHandler or other
-    } finally {
-        setLoading(false)
-    }
-}
-
-const search = () => {
-    fetchData({
-        ...basePagination,
-        ...formModel.value
-    } as unknown as ApiRequestLogParams)
-}
-const onPageChange = (current: number) => {
-    fetchData({ ...basePagination, current })
-}
-
-fetchData()
-
-watch(
-    () => columns.value,
-    val => {
-        cloneColumns.value = cloneDeep(val)
-        cloneColumns.value.forEach((item, index) => {
-            item.checked = true
-        })
-        showColumns.value = cloneDeep(cloneColumns.value)
-    },
-    { deep: true, immediate: true }
-)
+// 使用自定义的加载状态 Hook
+const { loading, setLoading } = useLoading(true)
+// 国际化函数
+const { t } = useI18n()
+// 定义渲染数据的响应式变量
+const renderData = ref<ApiRequestLog[]>([])
 
 // 接收父组件传递的 API 对象
 const props = defineProps<{
     api: ApiList | null
 }>()
 
-const init = () => {
-    console.log('ApiDoc 初始化')
-    // 其他初始化逻辑
+// 基础分页配置
+const basePagination: Pagination = {
+    current: 1, // 当前页
+    pageSize: 10, // 每页显示条数
+    showJumper: true,
+    showTotal: true, // 是否显示数据总数
+    showMore: true // 是否显示更多按钮
 }
+// 分页的响应式变量
+const pagination = reactive({
+    ...basePagination
+})
+// 获取数据的异步函数
+const fetchData = async (params: ApiRequestLogParams) => {
+    // 如果日期范围有效，直接使用字符串数组
+    if (formModel.value.createdTimeRange?.length === 2) {
+        params.createdTimeRange = formModel.value.createdTimeRange // 直接传递字符串数组
+    }
+
+    if (props.api && props.api.id) {
+        setLoading(true) // 开始加载
+        try {
+            params.id = undefined
+            params.apiId = props.api.id // 设置 API ID
+            const { data } = await apiRequestLogList(params) // 调用 API 获取数据
+            renderData.value = data.records // 设置渲染数据
+            pagination.current = params.current // 更新当前页
+            pagination.total = data.total // 更新总条数
+        } catch (err) {
+            console.error('数据获取失败:', err) // 错误日志
+        } finally {
+            setLoading(false) // 加载结束
+        }
+    }
+}
+
+// 搜索触发函数
+const search = () => {
+    fetchData({
+        current: basePagination.current,
+        pageSize: basePagination.pageSize,
+        ...formModel.value // 自动合并表单数据
+    })
+}
+const init = () => {
+    fetchData({
+        ...basePagination,
+        ...formModel.value
+    } as unknown as ApiRequestLogParams)
+}
+// 分页变化时的处理函数
+const onPageChange = (current: number) => {
+    fetchData({ ...basePagination, current })
+}
+
 // 使用 defineExpose 公开 init 方法
 defineExpose({ init })
+
+// 在组件挂载时获取数据
+onMounted(() => {
+    init()
+})
+
+// 监听 api 对象的变化并重新调用 init 方法
+watch(
+    () => props.api,
+    (newApi, oldApi) => {
+        if (newApi !== oldApi) {
+            init()
+        }
+    }
+)
+</script>
+
+<script lang="ts">
+export default {
+    name: 'DataSource'
+}
 </script>
 
 <style scoped lang="less">
-.api-log {
-    padding: 16px;
-    background-color: #f9f9f9;
-    border-radius: 8px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    font-family: Arial, sans-serif;
-    color: #333;
+.container {
+    padding: 0 20px 20px 20px;
+}
 
-    p {
-        margin: 8px 0;
-        font-size: 14px;
-        line-height: 1.6;
-
-        &:first-of-type {
-            font-weight: bold;
-            font-size: 16px;
+:deep(.arco-table-th) {
+    &:last-child {
+        .arco-table-th-item-title {
+            margin-left: 16px;
         }
-    }
-
-    .highlight {
-        color: #1a73e8;
-        font-weight: bold;
-    }
-
-    .timestamp {
-        font-size: 12px;
-        color: #666;
     }
 }
 
+.action-icon {
+    margin-left: 12px;
+    cursor: pointer;
+}
+
+.active {
+    color: #0960bd;
+    background-color: #e3f4fc;
+}
+
+.nav-btn {
+    margin-right: 5px;
+}
+
+.setting {
+    display: flex;
+    align-items: center;
+    width: 200px;
+
+    .title {
+        margin-left: 12px;
+        cursor: pointer;
+    }
+}
+
+.username-block {
+    display: flex;
+    align-items: center;
+
+    span {
+        margin-left: 5px;
+    }
+}
 .no-api-doc {
     p {
         color: #888;
